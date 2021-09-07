63b8d8b6880a1612f1ec88751db46f2bdaa31896
Afiq Nazrie <57625126+mangkoran@users.noreply.github.com>
2021-09-07 20:36:17 +0800

    Code refactor (#1)
    Set `WSL_DISTRO_NAME` as default for `DOCKER_DISTRO`, fix `mkdir` file mode, add `-d` after `wsl.exe`

--- docker-service.sh
+++ docker-service.sh
@@ -32,7 +32,7 @@
 #
 # wsl -d Ubuntu ~/bin/docker-service Ubuntu
 
-[ -z $1 ] && DOCKER_DISTRO="" || DOCKER_DISTRO="-d $1"
+[ -z "$1" ] && DOCKER_DISTRO="$WSL_DISTRO_NAME" || DOCKER_DISTRO="$1"
 # If embedding this in .bashrc, .profile, .zshenv, or the like, remove the above line
 # and set $DOCKER_DISTRO to an empty string or something like this:
 # DOCKER_DISTRO="-d Ubuntu"
@@ -40,8 +40,7 @@ DOCKER_DIR=/mnt/wsl/shared-docker
 DOCKER_SOCK="$DOCKER_DIR/docker.sock"
 export DOCKER_HOST="unix://$DOCKER_SOCK"
 if [ ! -S "$DOCKER_SOCK" ]; then
-    mkdir -pm o=,ug=rw "$DOCKER_DIR"
+    mkdir -pm o=,ug=rwx "$DOCKER_DIR"
     chgrp docker "$DOCKER_DIR"
-    /mnt/c/Windows/System32/wsl.exe $DOCKER_DISTRO sh -c "nohup sudo -b dockerd < /dev/null > $DOCKER_DIR/dockerd.log 2>&1"
+    /mnt/c/Windows/System32/wsl.exe -d $DOCKER_DISTRO sh -c "nohup sudo -b dockerd < /dev/null > $DOCKER_DIR/dockerd.log 2>&1"
 fi
-
